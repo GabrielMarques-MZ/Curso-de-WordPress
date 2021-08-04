51aafb077296c8919dda1f29cc59480b74e3c417
Gabriel Marques <bielmarques922@gmail.com>
2021-08-04 11:35:50 -0300

    11 - Os Tres Blocos Basicos de um Tema WordPress (parte 1)

--- index.php
+++ index.php
@@ -6,7 +6,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Curso WordPress</title>
 </head>
-<body>
+<body <?php body_class(); ?> >
     <p>Este é uum paragarfo teste.</p>
+
+    <header>
+        <section class="top-bar">
+            <div class="social-media-icons"></div>
+            <div class="search"></div>
+        </section>
+        <section class="menu-area">
+            <section class="logo"></section>
+            <nav class="menu"></nav>
+        </section>
+    </header>
+
+    <div class="content-area">
+        <main>
+            <section class="slide"></section>
+            <section class="services"></section>
+            <section class="middle-area">
+                <asise class="sidebar"></asise>
+                <div class="news"></div>
+            </section>
+            <section class="map"></section>
+        </main>
+
+        <footer></footer>
+    </div>
 </body>
 </html>
